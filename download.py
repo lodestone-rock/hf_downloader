1dc840eb903694b92d78dcbeac6b09d985dd12ae
lodestone-rock <lodestone.rock@gmail.com>
2024-06-13 09:54:21 +0700

    check for hf model link

--- download.py
+++ download.py
@@ -3,29 +3,31 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from huggingface_hub import HfFileSystem
 import os
 
+
 def construct_file_list(fs, path):
     """
     Recursively constructs a flat list of file paths in a Hugging Face repository.
-    
+
     Args:
     - fs (HfFileSystem): The Hugging Face file system object.
     - path (str): The current path to explore.
-    
+
     Returns:
     - list: A list of file paths in the repository.
     """
     file_list = []
-    
+
     for entry in fs.ls(path):
-        name = entry['name']
-        entry_type = entry['type']
-        
-        if entry_type == 'directory':
+        name = entry["name"]
+        entry_type = entry["type"]
+
+        if entry_type == "directory":
             file_list.extend(construct_file_list(fs, name))
         else:
             file_list.append(name)
-    
+
     return file_list
+
 
 def get_hf_repo_url(repo_id: str) -> list:
     format = f"https://huggingface.co/{repo_id}/resolve/main/"
@@ -35,9 +37,14 @@ def get_hf_repo_url(repo_id: str) -> list:
 
     urls = []
     for path in flat_paths:
-        urls.append([format + path.replace(repo_id + "/", ""), "/".join(path.split("/")[2:-1])])    
+        if repo_id.split("/")[0] == "datasets":
+            folder = "/".join(path.split("/")[2:-1])
+        else:
+            folder = "/".join(path.split("/")[1:-1])
+        urls.append([format + path.replace(repo_id + "/", ""), folder])
 
     return urls
+
 
 def generate_wget_commands(urls_and_paths, auth_token=None):
     wget_commands = []
@@ -48,19 +55,34 @@ def generate_wget_commands(urls_and_paths, auth_token=None):
             token = f'--header="Authorization: Bearer {auth_token}"'
         else:
             token = ""
-        wget_command = f'wget {token} -nd -P {path} {url}'
+        wget_command = f"wget {token} -nd -P {path} {url}"
         wget_commands.append(wget_command)
     return wget_commands
+
 
 def execute_wget_command(cmd):
     os.system(cmd)
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Process HF repo paths.')
-    parser.add_argument('hf_path', type=str, help='The Hugging Face repository path.')
-    parser.add_argument('--flag', type=str, default='download', choices=['download', 'dump'], help='Choose "download" to execute wget commands or "dump_command" to dump wget commands to a txt file.')
-    parser.add_argument('--workers', type=int, default=2, help='Number of worker threads for concurrency.')
-    parser.add_argument('--auth_token', type=str, help='Number of worker threads for concurrency.')
+    parser = argparse.ArgumentParser(description="Process HF repo paths.")
+    parser.add_argument("hf_path", type=str, help="The Hugging Face repository path.")
+    parser.add_argument(
+        "--flag",
+        type=str,
+        default="download",
+        choices=["download", "dump"],
+        help='Choose "download" to execute wget commands or "dump_command" to dump wget commands to a txt file.',
+    )
+    parser.add_argument(
+        "--workers",
+        type=int,
+        default=2,
+        help="Number of worker threads for concurrency.",
+    )
+    parser.add_argument(
+        "--auth_token", type=str, help="Number of worker threads for concurrency."
+    )
 
     args = parser.parse_args()
 
@@ -68,17 +90,20 @@ def main():
     wget_cmd = generate_wget_commands(urls_and_paths, args.auth_token)
 
     try:
-        if args.flag == 'download':
+        if args.flag == "download":
             with ThreadPoolExecutor(max_workers=args.workers) as executor:
-                futures = [executor.submit(execute_wget_command, cmd) for cmd in wget_cmd]
+                futures = [
+                    executor.submit(execute_wget_command, cmd) for cmd in wget_cmd
+                ]
                 for future in as_completed(futures):
                     future.result()
-        elif args.flag == 'dump':
-            with open('wget_commands.txt', 'w') as f:
-                f.write('\n'.join(wget_cmd))
+        elif args.flag == "dump":
+            with open("wget_commands.txt", "w") as f:
+                f.write("\n".join(wget_cmd))
     except KeyboardInterrupt:
         print("\nReceived KeyboardInterrupt, stopping execution.")
         # You may want to add cleanup code here if needed
+
 
 if __name__ == "__main__":
     main()
